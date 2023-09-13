8e640563f10b59b17291e781360f3c1701ec4ed8
celu32 <celu_32@hotmail.com>
2023-09-13 12:04:02 -0300

    SE AGREGA FUNCIONALIDAD DE CAMBIO DE COLOR DEL CORAZON DE FAVORITOS

--- examen_final/src/routes/Favs.jsx
+++ examen_final/src/routes/Favs.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
 import Card from "../components/Card";
-import React, { useContext } from "react";
-import { Context } from "../context/ContextProvider";
-
+import React, { useState , useEffect } from "react";
 
 const Favs = () => {
+    
+  const localStorageFav = localStorage.getItem('fav');
 
-
-
+  let odontologos = JSON.parse(localStorageFav) || [];
+  
   return (
     <>
-      <h1>Dentists Favs</h1>
+      <h1>Favoritos</h1>
       <div className='card-grid'>
-       
+      {odontologos.map(odontologo => (
+          <Card key={odontologo.id} name={odontologo.name} username={odontologo.username} id={odontologo.id} />
+        ))}
       </div>
     </>
   );
